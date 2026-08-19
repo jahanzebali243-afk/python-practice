2628d817d22a9db3e5790758470122cc89a6f4a2
Jahanzeb <jahanzebali243@gmail.com>
2026-08-19 11:10:25 +0500

    Add even or odd checker function

---
+++ Function_for_ODD_Even.py
@@ -0,0 +1,9 @@
+#Find given number is even or odd
+
+def even_or_odd(num):
+    if num %2 == 0 :
+      return  "Given number is Even number"
+    else:
+      return "Given numer is Odd number"
+
+print (even_or_odd(72342))
