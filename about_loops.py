348133bcc6897cb11f0c2d5287c4d8d2340937c1
Jahanzeb <jahanzebali243@gmail.com>
2026-09-05 16:54:03 +0500

    Add some loops counditions and uses of it

---
+++ about_loops.py
@@ -0,0 +1,48 @@
+#1. Simple Iteration (fixed )
+N = 4 
+for i in range(N):
+    print ("Pakistan")
+
+#2. Accumulator Pattern (for building results)
+result = 5   # sum ke liye 0, product ke liye 1
+for i in range(10):
+    result = result + i    # ya *, ya koi bhi operation
+print(result)
+
+#3. Filter Pattern (checking coundition)
+collection = 23
+for item in collection:
+    if collection /2 :
+        pass
+        # us item pe kaam karo
+
+#4. Search + Flag Pattern (Finding something)
+target = 5
+collection = 4
+found = False
+for item in collection:
+    if item == target:
+        found = True
+        break
+if found:
+    print("Found")
+else:
+    print("Not Found")
+    pass
+
+#5.Nested Loop (loop ke andar loop)
+for i in range("rows"):
+    for j in range("col"):
+         pass
+
+#6. While with Counter (condition + manual update)
+i = 10
+while i>8:
+    i % 2 == 0
+    print (i)
+    i = i + 1    
+    pass
+
+#7. Reverse/Step Loop
+for i in range(9, 90, 10):
+    print (i)
