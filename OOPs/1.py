7255103cb5b62da37813077f249b6dd12646b830
Jahanzeb <jahanzebali243@gmail.com>
2026-09-13 20:37:50 +0500

    Practice of OOPs

---
+++ OOPs/1.py
@@ -0,0 +1,7 @@
+class Dog :
+    def __init__(self , name , color):
+        self.name = name
+        self.color = color
+
+my_dog = Dog("Boob" , "Brown")
+print (my_dog.name , my_dog.color)
---
+++ OOPs/2.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self, account_holder, balance=0):
+        self.account_holder = account_holder
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited {amount}. New balance: {self.balance}")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient balance")
+        else:
+            self.balance -= amount
+            print(f"Withdrew {amount}. New balance: {self.balance}")
+
+    def check_balance(self):
+        print(f"Current balance: {self.balance}")
