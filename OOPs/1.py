fc37d158e50c847703b0066c62c2c4c8da7d8184
Jahanzeb <jahanzebali243@gmail.com>
2026-09-16 18:40:34 +0500

    Practice of OOPs

--- OOPs/1.py
+++ OOPs/1.py
@@ -1,7 +1,9 @@
 class Dog :
-    def __init__(self , name , color):
+    def __init__(self , name , color , number , parents_status):
         self.name = name
         self.color = color
+        self.number = number
+        self.parents_status = parents_status
 
-my_dog = Dog("Boob" , "Brown")
-print (my_dog.name , my_dog.color)
+my_dog = Dog("Boob" , "Brown" , 67 , "alive")
+print (my_dog.name , my_dog.color , my_dog.number , my_dog.parents_status)
