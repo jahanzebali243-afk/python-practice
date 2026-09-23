410176e64b99449882ec7f37f0abeacbe5b7e25d
Jahanzeb <jahanzebali243@gmail.com>
2026-09-23 23:43:48 +0500

    solve that student exercise

--- OOPs/5.py
+++ OOPs/5.py
@@ -4,10 +4,11 @@ class student :
     def __init__(self ,name , total_marks):
         self.name = name 
         self.total_marks = total_marks
-        def average (self ):
-           sum = 0
-           for  eachValue in self.total_marks:
-             sum = sum+eachValue
+    def average (self ):
+        sum = 0
+        for  eachValue in self.total_marks:
+            sum = sum+eachValue
+               
         average =  sum/3
         print (f"Average of your marks is : {average}")
 
